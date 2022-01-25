1bc4190c54efda9ee1bab99645b4f79e4c68d812
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-01-25 15:10:17 -0600

    Add files via upload

---
+++ BasicFuncPart/BasicFuncPart/Player.h
@@ -0,0 +1,31 @@
+class Player{
+  public:
+    Player(){
+		lastLocation = NULL;
+		currentLocation = NULL;
+	}
+
+	void setCurrent(areaNode* loc){
+        lastLocation = currentLocation;
+		currentLocation = loc;
+	}
+
+	bool playerMoved(){
+		return !(lastLocation == currentLocation);
+	}
+	areaNode* getCurrent(){
+		return currentLocation;
+	}
+
+	//isGameOver should return: 0 for continue, 1 for win, 2 for lose
+	int isGameOver(){
+        if(currentLocation->info.getGoal() && !currentLocation->info.getID()) return 1;
+        else if(!currentLocation->info.getGoal() && currentLocation->info.getID()) return 2;
+        return 0;
+	}
+
+  private:
+		areaNode* currentLocation;
+		areaNode* lastLocation;
+
+};
